33b77338718a9c7b468c2269d7cae72a82fd2cce
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-02-05 22:02:14 +0700

    add file

---
+++ tugas17.js
@@ -0,0 +1,26 @@
+// TUGAS 17-1
+function tugas17(){
+  var x = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+  var y = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+  console.log("Sebelum diubah:")
+  console.log(x);
+  console.log(y);
+
+  console.log("Setelah diubah:")
+  console.log(x.toLowerCase());
+  console.log(y.toUpperCase());
+}
+tugas17();
+
+// TUGAS17-2
+function tugas172(){
+  var x = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+  console.log(x.charCodeAt(0))
+  console.log(x.charCodeAt(1));
+  console.log(x.charCodeAt(2));
+  console.log(x.charCodeAt(3));
+  console.log(x.charCodeAt(4));
+  console.log(x.charCodeAt(5));
+}
+tugas172();
